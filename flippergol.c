e4a6f88fa1747e48aaeb92fd58ff8ac5df3550f3
Martin Ochs <martin.ochs@mad-mo.de>
2025-01-21 20:46:55 +0100

    Made the array storage so that the app doesn't crash anymore when using the whole screen

--- flippergol.c
+++ flippergol.c
@@ -1,18 +1,12 @@
 #include <furi.h>
 #include <gui/gui.h>
 
-#define WIDTH    32 // 128
-#define HEIGHT   32 // 64
+#define WIDTH    128
+#define HEIGHT   64
 
 static volatile int exit_app;
-static uint8_t grid[WIDTH*HEIGHT/8];
-static uint8_t new_grid[WIDTH*HEIGHT/8];
-
-
-
-#define SET_ARRAY_BIT(array, x, y)   array[((x*WIDTH)+y)/8] |=  (1 << (((x*WIDTH)+y)%8))
-#define CLR_ARRAY_BIT(array, x, y)   array[((x*WIDTH)+y)/8] &= ~(1 << (((x*WIDTH)+y)%8))
-#define GET_ARRAY_BIT(array, x, y) ((array[((x*WIDTH)+y)/8] &   (1 << (((x*WIDTH)+y)%8))) ? 1 : 0)
+static uint8_t grid[WIDTH][HEIGHT];
+static uint8_t new_grid[WIDTH][HEIGHT];
 
 
 
@@ -25,15 +19,12 @@ void init_grid(void)
         {
             if(random() & 1)
             {
-                SET_ARRAY_BIT(grid, x, y);
+                grid[x][y] = 1;
             }
             else
             {
-                CLR_ARRAY_BIT(grid, x, y);
+                grid[x][y] = 0;
             }
-if(x==y) SET_ARRAY_BIT(grid, x, y);
-if(x==0) SET_ARRAY_BIT(grid, x, y);
-if(y==0) SET_ARRAY_BIT(grid, x, y);
         }
     }
 }
@@ -50,7 +41,7 @@ static void draw_grid_callback(Canvas* canvas, void* context)
     {
         for(y=0; y<HEIGHT; y++)
         {
-            if(GET_ARRAY_BIT(grid, x, y))
+            if(grid[x][y])
             {
                 canvas_draw_dot(canvas, x, y);
             }
@@ -63,7 +54,6 @@ static void draw_grid_callback(Canvas* canvas, void* context)
 void update_grid(void)
 {
     int x, y;
-
     for(x=0; x<WIDTH; x++)
     {
         for(y=0; y<HEIGHT; y++)
@@ -79,34 +69,26 @@ void update_grid(void)
                     }
                     int nx = (x + dx) % WIDTH;
                     int ny = (y + dy) % HEIGHT;
-                    neighbors += GET_ARRAY_BIT(grid, nx, ny);
+                    neighbors += grid[nx][ny];
                 }
             }
-            if     ((GET_ARRAY_BIT(grid, x, y) == 1) && (neighbors  < 2)) // Underpopulation
+            if     ((grid[x][y] == 1) && (neighbors  < 2)) // Underpopulation
             {
-                CLR_ARRAY_BIT(new_grid, x, y);
+                new_grid[x][y] = 0;
             }
-            else if((GET_ARRAY_BIT(grid, x, y) == 1) && (neighbors  > 3)) // Overpopulation
+            else if((grid[x][y] == 1) && (neighbors  > 3)) // Overpopulation
             {
-                CLR_ARRAY_BIT(new_grid, x, y);
+                new_grid[x][y] = 0;
             }
-            else if((GET_ARRAY_BIT(grid, x, y) == 0) && (neighbors == 3)) // Reproduction
+            else if((grid[x][y] == 0) && (neighbors == 3)) // Reproduction
             {
-                SET_ARRAY_BIT(new_grid, x, y);
+                new_grid[x][y] = 1;
             }
             else                                           // Stasis
             {
-                if(GET_ARRAY_BIT(grid, x, y))
-                {
-                    SET_ARRAY_BIT(new_grid, x, y);
-                }
-                else
-                {
-                    CLR_ARRAY_BIT(new_grid, x, y);
-                }
+                new_grid[x][y] = grid[x][y];
             }
         }
-
     }
 
     memcpy(grid, new_grid, sizeof(grid));
